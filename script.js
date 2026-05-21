cfcff3b1be599ac53fc6e1b15a806accc101e328
Frederic Briand <fbr91144@gmail.com>
2026-05-21 18:05:42 +0200

    prompt var

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello wilders!");
+
+const firstValue = prompt("Premier nombre ?");
+const operator = prompt("Opérateur ?");
+const secondValue = prompt("Deuxième nombre ?");
+
+console.log("Bye wilders!");
