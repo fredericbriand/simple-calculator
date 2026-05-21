7f2debdd2f3064386b4fc8d6e1821ae6b7d9f908
Frederic Briand <fbr91144@gmail.com>
2026-05-21 18:56:01 +0200

    calculator complete

--- script.js
+++ script.js
@@ -3,5 +3,19 @@ console.log("Hello wilders!");
 const firstValue = prompt("Premier nombre ?");
 const operator = prompt("Opérateur ?");
 const secondValue = prompt("Deuxième nombre ?");
+let result;
 
+if (operator === "+") {
+    result = parseInt(firstValue) + parseInt(secondValue);
+} else if (operator === "-") {
+    result = parseInt(firstValue) - parseInt(secondValue);
+} else if (operator === "*") {
+    result = parseInt(firstValue) * parseInt(secondValue);
+} else if (operator === "/") {
+    result = parseInt(firstValue) / parseInt(secondValue);
+}  else {
+    result = "Opération inconnue";
+}
+
+console.log(result);
 console.log("Bye wilders!");
